d349bcc4de40607dcf040c0b7aae50e7bb5cdd06
SergeyOlifir <Sergey.Olifir@gmail.com>
2014-03-01 17:07:03 +0200

    Add messages

---
+++ fuel/app/migrations/008_create_messages.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_messages
+{
+	public function up()
+	{
+		\DBUtil::create_table('messages', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'user_id' => array('constraint' => 11, 'type' => 'int'),
+			'message' => array('type' => 'text'),
+			'type' => array('constraint' => 50, 'type' => 'varchar'),
+			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+
+		), array('id'));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('messages');
+	}
+}
