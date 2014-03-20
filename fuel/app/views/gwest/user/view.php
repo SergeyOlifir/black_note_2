089edd1713cb7176acb520df3e295635c7b02753
SergeyOlifir <Sergey.Olifir@gmail.com>
2014-03-20 13:31:36 +0200

    fix user view

--- fuel/app/views/gwest/user/view.php
+++ fuel/app/views/gwest/user/view.php
@@ -17,25 +17,25 @@
             <div class="form-group">
                 <label class="col-sm-2 control-label">Дата рождения:</label>
                 <div class="col-sm-10">
-                    <p class="form-control-static"><?= $user->userage; ?></p>
+                    <p class="form-control-static"><?= isset($user->userage) ? $user->userage : ''; ?></p>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-sm-2 control-label">Откуда:</label>
                 <div class="col-sm-10">
-                    <p class="form-control-static"><?= Model_Country::find($user->country_id)->title;  ?> &nbsp <?= Model_Region::find($user->region_id)->title;  ?> обл. &nbsp; г. <?= Model_Sity::find($user->sity_id)->title; ?></p>
+                    <p class="form-control-static"><?= isset($user->country_id) ? Model_Country::find($user->country_id)->title : '';  ?> &nbsp <?= isset($user->region_id) ? Model_Region::find($user->region_id)->title : '';  ?> обл. &nbsp; г. <?= isset($user->sity_id) ? Model_Sity::find($user->sity_id)->title : ''; ?></p>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-sm-2 control-label">О себе:</label>
                 <div class="col-sm-10">
-                    <p class="form-control-static"><?= $user->abaut; ?></p>
+                    <p class="form-control-static"><?= isset($user->abaut) ? $user->abaut : ''; ?></p>
                 </div>
             </div>
             <div class="form-group">
                 <label class="col-sm-2 control-label">Интересы, вид деятельности:</label>
                 <div class="col-sm-10">
-                    <p class="form-control-static"><?= $user->job; ?></p>
+                    <p class="form-control-static"><?= isset($user->job) ? $user->job : ''; ?></p>
                 </div>
             </div>
         
